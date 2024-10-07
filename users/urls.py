9c93ce6125f0378adcd11e80d6762a142daf7901
Ben <32617030+BenZorg@users.noreply.github.com>
2024-10-07 14:10:34 +0100

    Added login and logout features

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,8 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 app_name = 'users'
 
 urlpatterns = [
-    path('signup/', views.signup_view, name="signup")
+    path('signup/', views.signup_view, name="signup"),
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout"),
 
 ] + debug_toolbar_urls()
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
 
@@ -7,9 +8,27 @@ def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             #log user in
+            login(request, user)
             return redirect('posts:list')
     else:       
         form = UserCreationForm()
     return render(request, 'users/signup.html', {'form':form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            #log user in
+            user = form.get_user()
+            login(request, user)
+            return redirect('posts:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('posts:list')
